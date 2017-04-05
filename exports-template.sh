3211f132c7b868c71a01eaf976cb4349c2dba3bc
Corentin Dupont <corentin.dupont@gmail.com>
2017-04-05 16:02:26 +0200

    putting back exports-template as before

--- exports-template.sh
+++ exports-template.sh
@@ -1,4 +1,4 @@
-export REACT_APP_DASHBOARD_IDENTITY=false
+export REACT_APP_DASHBOARD_IDENTITY=true
 export REACT_APP_ADMIN_USER=admin
-export REACT_APP_ADMIN_PASS=admin
+export REACT_APP_ADMIN_PASS=<admin-pass>
 export REACT_APP_KC_URL=http://localhost:8080/auth
