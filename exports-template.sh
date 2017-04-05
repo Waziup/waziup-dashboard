9614315fd1b0bbb43418de6262cb83c91ddb567f
malishahi <mehdi.alishahi@gmail.com>
2017-04-05 15:27:14 +0200

    historical data with D3

--- exports-template.sh
+++ exports-template.sh
@@ -1,4 +1,4 @@
-export REACT_APP_DASHBOARD_IDENTITY=true
+export REACT_APP_DASHBOARD_IDENTITY=false
 export REACT_APP_ADMIN_USER=admin
-export REACT_APP_ADMIN_PASS=<admin-password>
-export REACT_APP_KC_URL=http://aam.waziup.io/auth
+export REACT_APP_ADMIN_PASS=admin
+export REACT_APP_KC_URL=http://localhost:8080/auth
