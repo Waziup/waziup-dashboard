9cfa17ef0eedcdff16c49e8fc7b89b5ef3322701
malishahi <mehdi.alishahi@gmail.com>
2017-04-18 10:29:49 +0200

    addressed the problem with sensor data

--- src/components/sensors/sensorDetail/sensorDetail.js
+++ src/components/sensors/sensorDetail/sensorDetail.js
@@ -1,14 +1,13 @@
+import axios from 'axios'
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import { Container,  Col, Visible, Hidden } from 'react-grid-system'
 import {List, ListItem} from 'material-ui/List';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import node from './barChart';
 import rd3 from 'react-d3-library';
 import SensorData from '../../SensorData.js'
 import UTIL from '../../../utils.js'
 const BarChart = rd3.BarChart;
-
 
 var position = [12.238, -1.561];
 class sensorDetail extends Component {
@@ -20,7 +19,7 @@ class sensorDetail extends Component {
             dateCreated: "not available",
             markers: [],
             id:this.props.params.sensorId,
-            d3: '',
+            historicalData: []
         };
       }
  
@@ -29,7 +28,6 @@ class sensorDetail extends Component {
   };
 
   componentWillReceiveProps(nextProps){
-
     if (nextProps.sensors && this.props.params.sensorId) {
         var sensor = nextProps.sensors.find((el)=>{
             return el.id === this.props.params.sensorId;
@@ -57,10 +55,58 @@ class sensorDetail extends Component {
         this.setState({markers:markers})
 
     }
+
+    console.log('in propos sensor: ' + JSON.stringify(this.state.sensor));
+
   }
-  componentDidMount() {
-    this.setState({d3: node});
+
+ componentWillMount() {
+      var url='http://historicaldata.waziup.io/STH/v1/contextEntities/type/SensingDevice/id/Device_6/attributes/temperature';
+      console.log('sensor: ' + JSON.stringify(this.state.sensor));
+
+
+      axios.get(url, {
+        params: {'lastN': '10'},
+        headers: {
+        'Fiware-ServicePath':"/FL",
+        'Fiware-Service':"waziup",
+        "Accept": "application/json"
+        }})
+        .then((response)  => {
+          console.log(response);
+          var contextResponse0 = response.data.contextResponses[0];
+          const {contextElement: contextElement} = contextResponse0;
+          const attribute0 = contextElement.attributes[0];
+          const values = attribute0.values;
+          console.log("Temperature:" + attribute0.name);
+
+          const data = {}
+          data.dataSet = [];        
+        
+          for (var i in values) {
+              var value = values[i];
+              console.log(value.attrValue + "  ,  " + value.recvTime);
+              data.dataSet.push({"label": value.recvTime.toString(), "value":value.attrValue});
+          }
+          console.log("inside" + JSON.stringify(data.dataSet));  
+        
+          
+          data.margins = {top: 20, right: 20, bottom: 70, left: 40};
+          data.yAxisLabel = 'Temperature (Celsious)';
+          data.fill = [];
+          data.width = 960;
+          data.height = 700;
+          data.ticks = 10;
+          data.barClass = 'bar';
+          this.setState({historicalData: [data]});        
+      })
+      .catch((response) => {
+          console.log("ERROR");
+          console.log(response);
+          return;    
+      })
   }
+  
   render() {
     if (this.state.markers.lenght>0) {
     }
@@ -104,8 +150,10 @@ class sensorDetail extends Component {
             </CardText>
             
             <CardTitle title="Historical Data"/>
-            <CardText>
-                <BarChart data={this.state.d3} />
+            <CardText> {
+                //console.log("data at use: " + JSON.stringify(this.state.historicalData[0].dataSet))
+                }
+                <BarChart data={this.state.historicalData[0]} />
             </CardText>
           </Card>
         </Container>
