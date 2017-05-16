f492e04958fb143076007562a16bf73313bf6f63
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-16 11:52:40 +0200

    first commit

--- src/components/notification/NotificationFormContainer.js
+++ src/components/notification/NotificationFormContainer.js
@@ -4,11 +4,16 @@ import notification from './NotificationForm.js';
 
 
 function mapStateToProps(state) {
-    return {}
+   return {
+      notifications: state.notifications
+      currentUser: state.currentUser.currentUser,
+   }
 }
 
 function mapDispatchToProps(dispatch) {
-    return {}
+   return {
+      fetchNotifications:(service, servicePath)=>{dispatch(fetchNotifications(service, servicePath))}
+   }
 }
 const NotificationFormContainer = connect( mapStateToProps,mapDispatchToProps)(notification);
 export default NotificationFormContainer;
