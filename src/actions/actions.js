8636653895310bb56f085f980ace5343cdee7322
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-16 11:52:40 +0200

    first commit

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -67,6 +67,57 @@ export function fetchSensors(service, servicePath) {
         }
 };
 
+export function getHistoData(sensorId, measurement, service, servicePath) {
+    if (!servicePath) {servicePath = defaultServicePathQuery;}
+    if (!service)     {service     = defaultService;}
+    return function(dispatch) {
+          var url='http://historicaldata.waziup.io/STH/v1/contextEntities/type/SensingDevice/id/' + sensorId + '/attributes/' + measurement;
+          return axios.get(url,{
+              params: {'lastN': '24'},
+              headers: {
+                'content-type': 'application/json',
+                'fiware-servicepath': servicePath,
+                'fiware-service': service,
+              },
+            })
+            .then(function(response) {
+                //console.log(response);
+                const contextResponse0 = response.data.contextResponses[0];
+                  const { contextElement: contextElement } = contextResponse0;
+                  const attribute0 = contextElement.attributes[0];
+                  const values = attribute0.values;
+                  const data = [];
+                  for (var i in values) {
+                    const value = values[i];
+                    //console.log(value.attrValue + "  ,  " + value.recvTime); 
+                    //.toString().substring(11, 19)
+                    data.push({ time: value.recvTime, value: parseFloat(value.attrValue) });
+                  }
+                  if (data.length > 0) {
+                      dispatch(getHistoDataSuccess(measurement,data));
+                  }
+
+            })
+            .catch(function(response){
+              dispatch(getHistoDataError(response.data));
+            })
+        }
+};
+
+export function getHistoDataSuccess(measurementId,data) {
+    return{
+          type: types.GET_HISTORICAL_SUCCESS,
+          data: {measurementId:measurementId,json:data}
+    }
+};
+
+export function getHistoDataError(json) {
+    return {
+          type: types.GET_HISTORICAL_ERROR,
+          data: json
+        }
+};
+
 
 export function createSensor(sensor, service, servicePath) {
     return function(dispatch) {
@@ -281,88 +332,38 @@ export function updateUserError(json) {
         }
 };
 
-export function getHistoData(sensorId, measurement, service, servicePath) {
-    if (!servicePath) {servicePath = defaultServicePathQuery;}
-    if (!service)     {service     = defaultService;}
-    console.log(sensorId);
+export function getNotifications(servicePath,service) {
+    if (!servicePath) {servicePath = fiwareServicePathQuery;}
+    if (!service) {service = fiwareService;}
     return function(dispatch) {
-          var url='http://historicaldata.waziup.io/STH/v1/contextEntities/type/SensingDevice/id/' + sensorId + '/attributes/' + measurement;
-          return axios.get(url,{
-              params: {'lastN': '24'},
+
+          var url='http://orion.waziup.io/v2/subscriptions'
+          return axios.get(url, {
               headers: {
-                'content-type': 'application/json',
+                'content-type':'application/json',
                 'fiware-servicepath': servicePath,
                 'fiware-service': service,
               },
             })
             .then(function(response) {
-                console.log(response);
-                const contextResponse0 = response.data.contextResponses[0];
-                  const { contextElement: contextElement } = contextResponse0;
-                  const attribute0 = contextElement.attributes[0];
-                  const values = attribute0.values;
-                  const data = [];
-                  for (var i in values) {
-                    const value = values[i];
-                    //console.log(value.attrValue + "  ,  " + value.recvTime);
-                    data.push({ time: value.recvTime.toString().substring(11, 19), value: parseFloat(value.attrValue) });
-                  }
-                  if (data.length > 0) {
-                      dispatch(getHistoDataSuccess(measurement,data));
-                  }
-
+               dispatch(getNotificationsSuccess(response.data));
             })
             .catch(function(response){
-              dispatch(getHistoDataError(response.data));
+               dispatch(getNotificationsError(response.data));
             })
         }
 };
 
-export function getHistoDataSuccess(measurementId,data) {
+export function getNotificationsSuccess(data) {
     return{
-          type: types.GET_HISTORICAL_SUCCESS,
-          data: {measurementId:measurementId,json:data}
-    }
-};
-
-export function getHistoDataError(json) {
-    return {
-          type: types.GET_HISTORICAL_ERROR,
-          data: json
-        }
-};
-
-export function subscribeHistoData(sub, service, servicePath) {
-    if (!servicePath) {servicePath = defaultServicePathQuery;}
-    if (!service)     {service     = defaultService;}
-    return function(dispatch) {
-          var url='http://orion.waziup.io/v1/data/subscriptions'
-          return axios.get(url, sub, {
-              headers: {
-                'content-type': 'application/json',
-                'fiware-servicepath': servicePath,
-                'fiware-service': service,
-              },
-            })
-            .then(function(response) {
-              dispatch(getSubscribeSuccess(response.data));
-            })
-            .catch(function(response){
-              dispatch(getSubscribeError(response.data));
-            })
-        }
-};
-
-export function getSubscribeSuccess(data) {
-    return{
-          type: types.GET_SUBSCRIBE_SUCCESS,
+          type: types.GET_NOTIFICATIONS_SUCCESS,
           data: {json:data}
     }
 };
 
-export function getSubscribeError(json) {
+export function getNotificationsError(json) {
     return {
-          type: types.GET_SUBCRIBE_ERROR,
+          type: types.GET_NOTIFICATIONS_ERROR,
           data: json
         }
 };
