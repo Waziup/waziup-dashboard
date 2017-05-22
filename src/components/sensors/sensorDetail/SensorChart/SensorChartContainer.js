5cceeda3358d6199b031d5ca8cb7fe7ce4c4be7c
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-22 17:37:24 +0200

    notif action

--- src/components/sensors/sensorDetail/SensorChart/SensorChartContainer.js
+++ src/components/sensors/sensorDetail/SensorChart/SensorChartContainer.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import SensorChart from './SensorChart.js';
-import { getHistoData } from '../../../../actions/actions';
 
 function mapStateToProps(state) {
   return {
@@ -10,7 +9,6 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    getHistoData:(sensor,measurement,servicePath,service)=>{dispatch(getHistoData(sensor,measurement,servicePath,service))}
   }
 }
 const SensorChartContainer = connect( mapStateToProps,mapDispatchToProps)(SensorChart);
