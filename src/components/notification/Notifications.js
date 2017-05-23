41dd042c3f260527b78261dfa3e17652642fb234
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-23 18:24:06 +0200

    notif actions

--- src/components/notification/Notifications.js
+++ src/components/notification/Notifications.js
@@ -37,87 +37,74 @@ export default class NotificationForm extends Component {
        this.setState({notifications: nextProps.notifications})
     }
   }
-//Fire when submitting the form data
-handleSubmit(event) {
-  console.log("submit:" + JSON.stringify(event))
-  subscribeHistoData("WS_UPPA_Sensor2", [])
-    
-}
+  //Fire when submitting the form data
+  handleSubmit(event) {
+    console.log("submit:" + JSON.stringify(event))
+    subscribeHistoData("WS_UPPA_Sensor2", [])
+      
+  }
+  
+  //Fire the modal window when click on the button
+  handleOpen(event){
+      this.setState({modalOpen : true})
+  }
+  
+  handleClose(event){
+      this.setState({modalOpen : false})
+  }
+  
+  handleNotifDelete = (data) => {
+  }
 
-//Fire the modal window when click on the button
-handleOpen(event){
-    this.setState({modalOpen : true})
-}
+  handleNotifUpdate = (data) => {
+  }
 
-handleClose(event){
-    this.setState({modalOpen : false})
-}
-render() {
-
-const actions = [
-    <RaisedButton label="Cancel" primary={true} onTouchTap={this.handleClose}/>,
-    <RaisedButton label="Submit" primary={true} disabled={true} onTouchTap={this.handleClose}/>,
-];
-
-const rowDataSelector = (state, { griddleKey }) => {
-  return state
-    .get('data')
-    .find(rowMap => rowMap.get('griddleKey') === griddleKey)
-    .toJSON();
-};
-
-const enhancedWithRowData = connect((state, props) => {
-  return {
-    // rowData will be available into RowActions
-    rowData: rowDataSelector(state, props)
+  render() {
+  
+  const actions = [
+      <RaisedButton label="Cancel" primary={true} onTouchTap={this.handleClose}/>,
+      <RaisedButton label="Submit" primary={true} disabled={true} onTouchTap={this.handleClose}/>,
+  ];
+  
+  const rowDataSelector = (state, { griddleKey }) => {
+    return state
+      .get('data')
+      .find(rowMap => rowMap.get('griddleKey') === griddleKey)
+      .toJSON();
   };
-});
-
-
-return(
-    	<div>
-            <h1 className="page-title">Notifications settings</h1>
-                <Container>
-                    <FullWidthSection useContent={true} >      
-                        <Griddle resultsPerPage={50} data={this.state.notifications} plugins={[plugins.LocalPlugin]} showFilter={true} >
-                           <RowDefinition>
+  
+  const enhancedWithRowData = connect((state, props) => {
+    return {
+      // rowData will be available into RowActions
+      rowData: rowDataSelector(state, props),
+      deleteAction: this.handleNotifDelete,
+      updateAction: this.handleNotifUpdate
+    };
+  });
+  
+  return (
+     <div>
+           <h1 className="page-title">Notifications settings</h1>
+               <Container>
+                   <FullWidthSection useContent={true} >      
+                       <Card>                        
+                         <Griddle resultsPerPage={50} data={this.state.notifications} plugins={[plugins.LocalPlugin]} showFilter={true} >
+                            <RowDefinition>
                               <ColumnDefinition id="id" title="ID"/>
                               <ColumnDefinition id="description" title="Description"/>
                               <ColumnDefinition id="subject" title="Subject"/>
                               <ColumnDefinition id="actions" title="Actions" customComponent={enhancedWithRowData(RowActions)}/> 
-                           </RowDefinition>
-
-                        </Griddle>
-                        <Card>                        
-                          <Table fixedHeader={true} fixedFooter={true} selectable={true} multiSelectable={true} heigth='300px'>
-                            <TableHeader displaySelectAll={true} adjustForCheckbox={true} enableSelectAll={true}>                                 
-                                 <TableRow>
-                                    <TableHeaderColumn tooltip="The id of notification">ID</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="The username">Username</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="Channel">Channel</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="The user profile">Profile</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="The sensor">Sensor</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="The data from sensor">Data type</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="The user profile">Threshold</TableHeaderColumn>
-                                    <TableHeaderColumn tooltip="Actions">Actions</TableHeaderColumn>
-                                 </TableRow>
-                            </TableHeader>
-                            <TableBody>
-
-                            </TableBody>
-                           
-                          </Table>
-                          <CardActions>                           
-                            <RaisedButton label="Add" onTouchTap={this.handleOpen} primary={true}  />
-                            <RaisedButton label="Edit"  primary={true}/>
-                            <RaisedButton label="Delete" primary={true}/>
-                          </CardActions>
-                          </Card>                            
-                         <SubmitForm  modalOpen={this.state.modalOpen} handleClose={this.handleClose} onSubmit={this.handleSubmit} /> 
-                    </FullWidthSection>
-                </Container>
-           </div>
-        );
-    }
+                            </RowDefinition>
+                         </Griddle>
+                         <CardActions>                           
+                           <RaisedButton label="Add" onTouchTap={this.handleOpen} primary={true}  />
+                         </CardActions>
+                         </Card>                            
+                        <SubmitForm  modalOpen={this.state.modalOpen} handleClose={this.handleClose} onSubmit={this.handleSubmit} /> 
+                   </FullWidthSection>
+               </Container>
+        </div>
+     );
+  }
 }
 
