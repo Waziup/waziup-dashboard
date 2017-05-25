b8eef02f040f2af6133ab9342401764384510487
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-25 17:51:25 +0200

    delete notif

--- src/components/notification/RowActions.js
+++ src/components/notification/RowActions.js
@@ -21,10 +21,10 @@ class RowActions extends Component {
            <IconButton tooltip="View" tooltipPosition="top-center" containerElement={<Link to={"/notifications/" + rowData.id} />} >
               <View color={blue500}/>
            </IconButton> 
-           <IconButton tooltip="Update" tooltipPosition="top-center" onTouchTap={()=>{updateAction(rowData)}}>
+           <IconButton tooltip="Update" tooltipPosition="top-center" onTouchTap={()=>{updateAction(rowData.id)}}>
               <Update />
            </IconButton> 
-           <IconButton tooltip="Delete" tooltipPosition="top-center" onTouchTap={()=>{deleteAction(rowData)}}>
+           <IconButton tooltip="Delete" tooltipPosition="top-center" onTouchTap={()=>{deleteAction(rowData.id)}}>
               <Delete  color={red500}/>
            </IconButton>
         </div>
