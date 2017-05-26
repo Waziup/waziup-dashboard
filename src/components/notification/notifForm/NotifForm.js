3f3fe88bd6d8c0dee6cf37da5ac75a32a4bd08ec
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-26 15:30:35 +0200

    form fields

--- src/components/notification/notifForm/NotifForm.js
+++ src/components/notification/notifForm/NotifForm.js
@@ -5,6 +5,7 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import FlatButton from 'material-ui/FlatButton';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
 import UTIL from '../../../utils.js';
 import MenuItem from 'material-ui/MenuItem'
 import { RadioButton } from 'material-ui/RadioButton'
@@ -17,14 +18,13 @@ import {
 } from 'redux-form-material-ui'
 import {  Row, Col, Visible} from 'react-grid-system'
 import { initialize } from 'redux-form'
-// validation functions
 
 
 class notifForm extends Component {
   constructor(props){
     super(props);
     this.state = {
-      notif:{}
+      notif: {},
     };
   }
 
@@ -35,8 +35,8 @@ class notifForm extends Component {
   }
 
   render() {
-    const {pristine, reset, submitting,modalShowing, modalOpen,handleClose, onSubmit,formData} = this.props;
-      const actions = [
+    const {pristine, reset, submitting, modalShowing, modalOpen, handleClose, onSubmit, formData} = this.props;
+    const actions = [
       <FlatButton
         label="Cancel"
         primary={true}
@@ -55,6 +55,23 @@ class notifForm extends Component {
         }}
       />,
     ];
+    
+    const names = [
+       'Sensor1',
+       'Sensor2',
+     ];
+ 
+    function menuItems(values) {
+       return names.map((name) => (
+           <MenuItem
+             key={name}
+             insetChildren={true}
+             //checked={values && values.indexOf(name) > -1}
+             value={name}
+             primaryText={name}
+           />
+         ));
+       }
 
     return (
         <Dialog
@@ -67,11 +84,71 @@ class notifForm extends Component {
             >
           <form onSubmit={onSubmit}>
             <Row>
-                <Field name="description"
+                <Field name="desc"
                   component={TextField}
-                  hintText="description"
-                  floatingLabelText="Description"
+                  initialValues="notification for sensor xxx"
                   ref="description" withRef/>
+                <Field name="sensors"
+                  component={SelectField}
+                  multiple={true}
+                  hintText="sensors"
+                  floatingLabelText="Sensors"
+                  ref="sensors" withRef>
+                    <MenuItem value={1} primaryText="Sensor1" />
+                    <MenuItem value={2} primaryText="Sensor2" />
+                </Field>
+                <Field name="attrs"
+                  component={SelectField}
+                  multiple={true}
+                  value={["Sensor1", "Sensor2"]}
+                  hintText="attributes"
+                  floatingLabelText="Attributes"
+                  ref="sensors" withRef>
+                    <MenuItem value={1} primaryText="Attr1" />
+                    <MenuItem value={2} primaryText="Attr2" />
+                </Field>
+                <Field name="expr"
+                  component={TextField}
+                  value={"SM1>400"}
+                  hintText="expression"
+                  floatingLabelText="Expression"
+                  ref="expression" withRef>
+                </Field>
+                <Field name="url"
+                  component={TextField}
+                  value={"http://localhost"}
+                  hintText="URL"
+                  floatingLabelText="URL"
+                  ref="url" withRef>
+                </Field>
+                <Field name="headers"
+                  component={TextField}
+                  value={"headers"}
+                  hintText="Headers"
+                  floatingLabelText="Headers"
+                  ref="headers" withRef>
+                </Field>
+                <Field name="payload"
+                  component={TextField}
+                  value={"payload"}
+                  hintText="Payload"
+                  floatingLabelText="Payload"
+                  ref="payload" withRef>
+                </Field>
+                <Field name="expires"
+                  component={DatePicker}
+                  value={"expires"}
+                  hintText="Expires"
+                  floatingLabelText="Expires"
+                  ref="expires" withRef>
+                </Field>
+                <Field name="throttling"
+                  component={TextField}
+                  value={"throttling"}
+                  hintText="throttling"
+                  floatingLabelText="Throttling"
+                  ref="throttling" withRef>
+                </Field>
             </Row>
           </form>
         </Dialog>
@@ -88,6 +165,18 @@ notifForm = reduxForm({
 notifForm = connect(
   state => ({
     initialValues:{
+        "desc": "Send XXX when YYY",
+        "sensors": 1,
+        "attrs": 1, 
+        "expr": "SM1>400", 
+        "url": "https://api.plivo.com/v1/Account/MAMDA5ZDJIMDM1NZVMZD/Message/", 
+        "headers": {
+         "Content-type": "application/json",
+         "Authorization": "Basic TUFNREE1WkRKSU1ETTFOWlZNWkQ6TnpSbE5XSmlObVUyTW1GallXSmxPRGhsTlRrM01Ua3laR0V6TnpJeQ=="
+         }, 
+        "payload": "%7B%20%22src%22%3A%20%2200393806412092%22%2C%20%22dst%22%3A%20%2200923004075566%22%2C%20%22text%22%3A%20%22WaterSense%3A%20Field%20is%20too%20dry.%20${id}%20humidity%20value%20is%20${SM1}%20%22%7D", 
+        "expires": new Date("2040-05-24T20:00:00.00Z"), 
+        "throttling": 1, 
     }
   })
 )(notifForm)
