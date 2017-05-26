6e441484365d014be72780c1233a89a81fc00561
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-26 17:17:06 +0200

    formatting

--- src/components/notification/SubmitNotificationFormContainer.js
+++ src/components/notification/SubmitNotificationFormContainer.js
@@ -4,12 +4,14 @@ import notifsubmitform from './SubmitNotificationForm.js';
 
 
 function mapStateToProps(state) {
-    return {}
+    return {
+      sensors: state.sensors.sensors
+    }
 }
 
 function mapDispatchToProps(dispatch) {
     return {}
 }
-const SubmitNotificationFormContainer = connect( mapStateToProps,mapDispatchToProps)(notifsubmitform);
+const SubmitNotificationFormContainer = connect(mapStateToProps, mapDispatchToProps)(notifsubmitform);
 export default SubmitNotificationFormContainer;
 
