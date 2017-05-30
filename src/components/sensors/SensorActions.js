1981258235ecf43b7d3ed3ba4c8c6f9ff1a6cc15
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-30 23:31:58 +0200

    fix sensors list

--- src/components/sensors/SensorActions.js
+++ src/components/sensors/SensorActions.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from 'react';
 import IconButton from 'material-ui/IconButton';
 import { Link } from 'react-router';
@@ -7,19 +6,23 @@ import Update from 'material-ui/svg-icons/content/create';
 import View from 'material-ui/svg-icons/image/remove-red-eye';
 import {blue500, red500, greenA200} from 'material-ui/styles/colors';
 
-class RowActions extends Component {
+class SensorActions extends Component {
+  
+  constructor(props) {
+    super(props)
+  }
+
   render() {
-    let {deleteAction, updateAction} = this.props.metadata.customComponentMetadata;
-    let {rowData} = this.props;
+    let {rowData, deleteAction, updateAction} = this.props;
     return (
         <div>
            <IconButton tooltip="View" tooltipPosition="top-center" containerElement={<Link to={"/sensors/"+rowData.id} />} >
               <View color={blue500}/>
            </IconButton> 
-           <IconButton tooltip="Update" tooltipPosition="top-center" onTouchTap={()=>{updateAction(rowData)}}>
+           <IconButton tooltip="Update" tooltipPosition="top-center" onTouchTap={()=>{updateAction(rowData.id)}}>
               <Update />
            </IconButton> 
-           <IconButton tooltip="Delete" tooltipPosition="top-center" onTouchTap={()=>{deleteAction(rowData)}}>
+           <IconButton tooltip="Delete" tooltipPosition="top-center" onTouchTap={()=>{deleteAction(rowData.id)}}>
               <Delete  color={red500}/>
            </IconButton>
         </div>
@@ -27,4 +30,4 @@ class RowActions extends Component {
   }
 }
 
-export default RowActions;
+export default SensorActions;
