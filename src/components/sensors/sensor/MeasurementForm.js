52195a9980e645ea7f27c43035ae5e3f9c7cc982
Corentin Dupont <corentin.dupont@gmail.com>
2018-02-20 11:10:01 +0100

    measurement lists

--- src/components/sensors/sensor/MeasurementForm.js
+++ src/components/sensors/sensor/MeasurementForm.js
@@ -4,7 +4,8 @@ import Dialog from 'material-ui/Dialog';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem'
-import { SelectField, TextField } from 'redux-form-material-ui'
+import SelectField from 'material-ui/SelectField';
+import TextField from 'material-ui/TextField'
 import { Row, Col} from 'react-grid-system'
 import PropTypes from 'prop-types';
 import * as Waziup from 'waziup-js'
@@ -14,8 +15,13 @@ class MeasurementForm extends Component {
 
   constructor(props){
     super(props);
+    var defaultMeas = new Waziup.Measurement("TC")
+    defaultMeas.name = "My Temperature"
+    defaultMeas.dimension = "Temperature"
+    defaultMeas.unit = "DegreeCelsius"
+    defaultMeas.sensor_kind = "Thermometer"
     this.state = {
-      meas: {}
+      meas: defaultMeas 
     };
   }
   
@@ -23,6 +29,13 @@ class MeasurementForm extends Component {
     var meas = this.state.meas
     meas[formData.target.name] = formData.target.value;
     this.setState({meas: meas})
+  }
+  handleSelect = (field) => {
+    return (event, index, value) => {
+      var meas = this.state.meas
+      meas[field] = value;
+      this.setState({meas: meas});
+    }
   }
 
   render() {
@@ -34,11 +47,17 @@ class MeasurementForm extends Component {
       <Dialog title="New measurement" actions={actions} modal={true} open={this.props.modalOpen} autoScrollBodyContent={true}>
          <img src={sensorImage} height="100"/>
          <div className="locationCoords">
-           <TextField name="id"          floatingLabelText="ID"           value={this.state.meas.id}          onChange={this.handleChange}/>
-           <TextField name="name"        floatingLabelText="Name"         value={this.state.meas.name}        onChange={this.handleChange}/>
-           <TextField name="dimension"   floatingLabelText="Dimension"    value={this.state.meas.dimension}   onChange={this.handleChange}/>
-           <TextField name="unit"        floatingLabelText="Unit"         value={this.state.meas.unit}        onChange={this.handleChange}/>
-           <TextField name="sensor_kind" floatingLabelText="Sensor kind"  value={this.state.meas.sensor_kind} onChange={this.handleChange}/>
+           <TextField name="id" floatingLabelText="ID" value={this.state.meas.id} onChange={this.handleChange} title="Short ID used by the Gateway to send the measure"/>
+           <TextField name="name" floatingLabelText="Name" value={this.state.meas.name} onChange={this.handleChange} title="A name for this sensor measurement"/>
+           <SelectField name="sensor_kind" floatingLabelText="Sensor"  value={this.state.meas.sensor_kind} onChange={this.handleSelect("sensor_kind")} title="The kind of sensor used for this measurement">
+             {Waziup.SensingDevices.map(s => <MenuItem value={s} primaryText={s} />)}
+           </SelectField>
+           <SelectField name="dimension" floatingLabelText="Quantity kind"  value={this.state.meas.dimension} onChange={this.handleSelect("dimension")} title="What does it measures?">
+             {Waziup.QuantityKinds.map(s => <MenuItem value={s} primaryText={s} />)}
+           </SelectField>
+           <SelectField name="unit" floatingLabelText="Unit"  value={this.state.meas.unit} onChange={this.handleSelect("unit")} title="The measurement unit">
+             {Waziup.Units.map(s => <MenuItem value={s} primaryText={s} />)}
+           </SelectField>
         </div>
       </Dialog>
     );
