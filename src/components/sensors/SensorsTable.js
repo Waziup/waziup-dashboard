c0c5c88e1c0135d5056ac1011ec290ceb38f7951
Corentin Dupont <corentin.dupont@gmail.com>
2018-02-22 18:06:18 +0100

    sensor table

--- src/components/sensors/SensorsTable.js
+++ src/components/sensors/SensorsTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router';
 
 export default class SensorsTable extends Component {
   constructor(props) {
@@ -15,19 +15,39 @@ export default class SensorsTable extends Component {
     let sensor = this.props.sensor;
     return (
       <Table>
-        <TableHeader>
+        <TableHeader displaySelectAll={false}>
           <TableRow>
             <TableHeaderColumn>ID</TableHeaderColumn>
             <TableHeaderColumn>Name</TableHeaderColumn>
             <TableHeaderColumn>Owner</TableHeaderColumn>
+            <TableHeaderColumn>Measurements</TableHeaderColumn>
           </TableRow>
         </TableHeader>
-        <TableBody>
+        <TableBody displayRowCheckbox={false}>
         {this.props.sensors.map( sensor => (
           <TableRow key={sensor.id}>
-            <TableRowColumn>{sensor.id}</TableRowColumn>
-            <TableRowColumn>{sensor.name}</TableRowColumn>
-            <TableRowColumn>{sensor.owner}</TableRowColumn>
+            <TableRowColumn>
+              <Link to={"/sensors/" + sensor.id}>
+                {sensor.id}
+              </Link>
+            </TableRowColumn>
+            <TableRowColumn> {sensor.name} </TableRowColumn>
+            <TableRowColumn>{sensor.owner} </TableRowColumn>
+            <TableRowColumn>
+              <Table>
+                <TableBody displayRowCheckbox={false}>
+                  {sensor.measurements.map( meas => (
+                    <TableRow>
+                      <TableRowColumn> 
+                        <Link to={"/sensors/" + sensor.id + "/" + meas.id}>
+                          {(meas.name? meas.name: meas.id) + ": " + (meas.last_value? meas.last_value: "") + " " + (meas.unit? meas.unit: "")} 
+                        </Link>
+                      </TableRowColumn> 
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableRowColumn>
           </TableRow>
           ))}
         </TableBody>
