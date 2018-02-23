0a5e7b1de7023c8844435809245621b14253bc1f
Corentin Dupont <corentin.dupont@gmail.com>
2018-02-23 15:52:09 +0100

    iot ontology

--- src/components/sensors/sensor/MeasurementForm.js
+++ src/components/sensors/sensor/MeasurementForm.js
@@ -10,6 +10,7 @@ import { Row, Col} from 'react-grid-system'
 import PropTypes from 'prop-types';
 import * as Waziup from 'waziup-js'
 import sensorImage from '../../../images/gauge.png';
+import Divider from 'material-ui/Divider';
 
 class MeasurementForm extends Component {
 
@@ -37,12 +38,24 @@ class MeasurementForm extends Component {
       this.setState({meas: meas});
     }
   }
+  
+  getQuantityKinds = () => {
+    const currentSD = Waziup.SensingDevices.find(s => s.id == this.state.meas.sensor_kind)
+    return currentSD.QK.map(qkid => Waziup.QuantityKinds.find(qk => qk.id == qkid))
+  }
+  
+  getUnits = () => {
+    const currentQK = Waziup.QuantityKinds.find(qk => qk.id == this.state.meas.dimension)
+    return currentQK.units.map(uid => Waziup.Units.find(u => u.id == uid))
+  }
 
   render() {
     const actions = [
       <FlatButton label="Cancel" primary={true} onTouchTap={()=>{this.props.handleClose();}}/>,
       <FlatButton label="Submit" primary={true} onTouchTap={()=>{this.props.onSubmit(this.state.meas); this.props.handleClose();}}/>,
     ];
+
+
     return (
       <Dialog title={this.props.isEdit? "Edit Measurement": "Add measurement"} actions={actions} modal={true} open={this.props.modalOpen} autoScrollBodyContent={true}>
          <img src={sensorImage} height="100"/>
@@ -50,13 +63,13 @@ class MeasurementForm extends Component {
            <TextField name="id" disabled={this.props.isEdit} floatingLabelText="ID" value={this.state.meas.id} onChange={this.handleChange} title="Short ID used by the Gateway to send the measure"/>
            <TextField name="name" floatingLabelText="Name" value={this.state.meas.name} onChange={this.handleChange} title="A name for this sensor measurement"/>
            <SelectField name="sensor_kind" floatingLabelText="Sensor"  value={this.state.meas.sensor_kind} onChange={this.handleSelect("sensor_kind")} title="The kind of sensor used for this measurement">
-             {Waziup.SensingDevices.map(s => <MenuItem value={s} primaryText={s} />)}
+             {Waziup.SensingDevices.map(s => <MenuItem value={s.id} primaryText={s.label} />)}
            </SelectField>
            <SelectField name="dimension" floatingLabelText="Quantity kind"  value={this.state.meas.dimension} onChange={this.handleSelect("dimension")} title="What does it measures?">
-             {Waziup.QuantityKinds.map(s => <MenuItem value={s} primaryText={s} />)}
+             {this.getQuantityKinds().map(qk => <MenuItem value={qk.id} primaryText={qk.label} />)}
            </SelectField>
            <SelectField name="unit" floatingLabelText="Unit"  value={this.state.meas.unit} onChange={this.handleSelect("unit")} title="The measurement unit">
-             {Waziup.Units.map(s => <MenuItem value={s} primaryText={s} />)}
+             {this.getUnits().map(u => <MenuItem value={u.id} primaryText={u.label} />)}
            </SelectField>
         </div>
       </Dialog>
