6af0fadeafee8f3c5866c6d2f31561bd9f88e57c
Corentin Dupont <corentin.dupont@gmail.com>
2018-04-06 13:27:14 +0200

    better graph label

--- src/components/sensors/sensor/SensorChart.js
+++ src/components/sensors/sensor/SensorChart.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import UTIL from '../../../lib/utils.js';
 import { CustomTick, indexSelection } from '../../../lib/Visualization.js'
 import PropTypes from 'prop-types';
+import * as Waziup from 'waziup-js'
 
 class SensorChart extends Component {
   constructor(props) {
@@ -22,7 +23,7 @@ class SensorChart extends Component {
         <ResponsiveContainer width="100%" height={500} className="sensorChart">
           <LineChart data={data} margin={{ top: 5, right: 60, left: 0, bottom: 15 }}>
             <XAxis interval={0} domain={['dataMin', 'dataMax']} type="number" dataKey="time" tickFormatter={xFormatter} />
-            <YAxis label={{ value: meas.quantity_kind + " (" + meas.unit + ")", angle: -90, position: 'insideLeft' }}/>
+            <YAxis label={{ value: meas.quantity_kind + " (" + (meas.unit? Waziup.Units.getLabel(meas.unit): "") + ")", angle: -90, position: 'insideLeft' }}/>
             <Tooltip formatter={yFormatter} labelFormatter={xFormatter} />
             <CartesianGrid strokeDasharray="3 3" />
             <Legend align='right' verticalAlign='top' layout="vertical" wrapperStyle={{ right: '35px', top: '10px', border: '2px solid beige', padding: '5px 0px 5px 5px' }} />
