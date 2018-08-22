0159df2654ba39a14888accdd129d86e69399a7e
Corentin Dupont <corentin.dupont@gmail.com>
2018-08-22 22:08:30 +0200

    simplify gateway view

--- src/components/gateways/GatewaySensor.js
+++ src/components/gateways/GatewaySensor.js
@@ -35,31 +35,12 @@ export default class SensorLineCard extends Component {
                      modalOpen={this.state.modalEdit}
                      handleClose={() => this.setState({ modalEdit: false })}
                      onSubmit={s => this.props.updateSensorGatewayId(sensor.id, s.gateway_id)} />
-        <CardTitle>
-          <h2 className="cardTitle"> Node {(sensor.name? sensor.name + " ": "" ) + "(" + sensor.id + ")"} </h2>
-          {this.props.permission.scopes.includes("sensors:update")?
-            <EditIcon onClick={() => this.setState({modalEdit: true})}/>: null}
-        </CardTitle>
+        {this.props.permission && this.props.permission.scopes.includes("sensors:update")?
+                  <EditIcon onClick={() => this.setState({modalEdit: true})}/>: null}
         <Link to={'/sensors/' + sensor.id}> 
-          <div className="contentCards">
-            <div className={"boardIcon sensor" + sensor.id}>
-              <img src={sensorNodeImage} height="64" title={sensor.dateUpdated? "Last update at " + sensor.dateUpdated: "No data yet"}/>
-            </div>
-            {sensor.measurements.map(meas => {return (
-              <Card className={"card " + activeStyle(meas)}>
-                <div className="cardTitleDiv">
-                  <pre className="cardTitle"> {meas.name? meas.name : "(" + meas.id + ")"} </pre>
-                </div>
-                <div className="cardContent">
-                  <div className="measIcon">
-                    <MeasIcon sensing_device={meas.sensing_device} height="64" title={title(meas)}/>
-                  </div>
-                  <div className="measValue"> 
-                    <h3> {(meas.last_value? meas.last_value.value: "") + " " + (meas.unit? Waziup.Units.getLabel(meas.unit): "")} </h3>
-                  </div>
-                </div>
-              </Card>
-            )})}
+          <h2 className="cardTitle"> Node {(sensor.name? sensor.name + " ": "" ) + "(" + sensor.id + ")"} </h2>
+          <div className={"gatewayBoardIcon sensor" + sensor.id}>
+            <img src={sensorNodeImage} height="64" title={sensor.dateUpdated? "Last update at " + sensor.dateUpdated: "No data yet"}/>
           </div>
         </Link>
       </div>
