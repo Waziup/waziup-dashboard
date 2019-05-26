6f3b3c4ce83804fed050515a94f456aea4134df7
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2019-05-26 23:07:45 +0300

    add text for empty project list

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ProjectLineCard from './ProjectLineCard.js'
 import { Container } from 'react-grid-system';
-import { createProject, getProjects, updateProjectDevices, 
+import { createProject, createDevice, getProjects, updateProjectDevices, 
   getDevices, getGateways} from "../../actions/actions.js";
 import DOM from 'react-dom-factories';
 import { Link } from 'react-router';
@@ -11,6 +11,7 @@ import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import projectImage from '../../images/project.png';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 class Projects extends Component {
@@ -39,20 +40,33 @@ class Projects extends Component {
           </Toolbar>
         </AppBar>
         <ProjectForm
+          user={this.props.user}
           devices={this.props.devices}
           gateways={this.props.gateways}
           handleClose={() => this.setState({ modalAddProject: false })}
           modalOpen={this.state.modalAddProject}
+          createDevice={this.props.createDevice}
           onSubmit={s => this.props.createProject(s)}
         />
         <Button variant="contained" color="primary" className="addProjectButton" onTouchTap={() => this.setState({ modalAddProject: true })} >Create a project</Button>
         <br />
         <br />
-        { DOM.div(null, 
+        {
+          this.props.projects.length != 0 ? DOM.div(null, 
             this.props.projects.map((p,index) => 
             React.createElement(Link, { to: "/projects/" + p.id, key: { index } },
             React.createElement(ProjectLineCard, {project: p, updateProjectDevices: this.props.updateProjectDevices, permissions: this.props.permissions, key: (index)}))
-        ))
+        )) : 
+        <div>
+        <Paper style={{'padding':'20px'}}>
+          <Typography variant="h5" component="h3">
+          You don't have any projet.
+          </Typography>
+          <Typography component="p">
+            Get started by creating your own IoT project.
+          </Typography>
+        </Paper>
+      </div>
         }
       </Container>
     );
@@ -64,7 +78,8 @@ function mapStateToProps(state) {
     projects: state.projects.projects,
     permissions: state.permissions.project,
     devices: state.devices.devices,
-    gateways: state.gateways.gateways
+    gateways: state.gateways.gateways,
+    user: state.current_user,
   };
 }
 
@@ -72,6 +87,9 @@ function mapDispatchToProps(dispatch) {
   return {
     createProject: (project) => {
       dispatch(createProject(project));
+    },
+    createDevice: (device) => {
+      dispatch(createDevice(device));
     },
     getProjects: (params) => {dispatch(getProjects(params)) },
     updateProjectDevices: (sid, gid) => {dispatch(updateProjectDevices(sid, gid)) },
