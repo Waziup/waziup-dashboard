0fd8a9d5098413b5b1febc14ca9eee04224bb800
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2019-05-28 21:24:23 +0300

    add devices map for project detail

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,29 +1,38 @@
-import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { Container } from 'react-grid-system';
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Container } from "react-grid-system";
+import { Map as LeafletMap, Marker, Popup, TileLayer } from "react-leaflet";
+import { connect } from "react-redux";
+import { browserHistory } from "react-router";
+import ProjectNodeCard from "./ProjectNodeCard";
 import {
-  Map, Marker, Popup, TileLayer,
-} from 'react-leaflet';
-import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button';
-import { browserHistory } from 'react-router';
-import ProjectNodeCard from './ProjectNodeCard';
-import LocationForm from './LocationForm';
-import {
-  addSensor, deleteSensor, deleteProject, getProject, getProjects,
-  getDevices, getGateways, getDevicePermissions, getProjectPermissions, updateSensorName,
-   updateProjectName, updateProjectDevices, updateProjectGateways, createDevice
-} from '../../actions/actions.js';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import projectImage from '../../images/project.png';
+  deleteProject,
+  getProject,
+  getProjects,
+  getDevices,
+  getGateways,
+  getDevicePermissions,
+  getProjectPermissions,
+  updateProjectName,
+  updateProjectDevices,
+  updateProjectGateways,
+  createDevice
+} from "../../actions/actions.js";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import projectImage from "../../images/project.png";
 
 class ProjectDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = { modalLocation: false, fullProject: [] };
+    this.state = {
+      fullProject: [],
+      markers: [],
+      position: [14.4974, 14.4524],
+      locations: []
+    };
   }
 
   componentWillMount() {
@@ -35,46 +44,61 @@ class ProjectDetail extends Component {
     this.props.getDevices({ limit: 1000 });
     this.props.getGateways();
     var self = this;
-    const fullProject = (this.props.projects).find(function(project) {
+
+    const fullProject = this.props.projects.find(function(project) {
       return project.id == self.props.params.projectId;
     });
     this.setState({ fullProject: fullProject });
+    const markers = [];
+    const locations = [];
+    fullProject.devices.forEach(device => {
+      if (device.location) {
+        locations.push([device.location.latitude, device.location.longitude]);
+        markers.push(
+          <Marker
+            key={device.id}
+            position={[device.location.latitude, device.location.longitude]}
+          >
+            <Popup>
+              <span>
+                <a onClick={() => browserHistory.push(`/devices/${device.id}`)}>
+                  {" "}
+                  {device.id}
+                </a>
+              </span>
+            </Popup>
+          </Marker>
+        );
+      }
+    });
+    this.setState({ locations });
+    this.setState({ markers });
   }
 
   render() {
-    let renderElement = (
-      <h1>
-        {' '}
-        Project view is being loaded...
-        {' '}
-      </h1>
-    );
+    let renderElement = <h1> Project view is being loaded... </h1>;
     console.log(`sens:${JSON.stringify(this.props.project)}`);
     const project = this.props.project;
     if (project) {
-      const position = 
-      // project.devices[0] ? [
-      //   project.devices[0].location.latitude, project.devices[0].location.longitude,
-      // ] : 
-      [
-        12.238, -1.561,
-      ];
-      console.log(`pos:${JSON.stringify(position)}`);
       renderElement = (
         <Container fluid>
-        <AppBar position="static" style={{marginBottom: '30px',background: '#e9edf2'}}>
-          <Toolbar>
-          <img src={projectImage} height="50"/>
-            <Typography variant="h5" className="page-title">
-              Project Detail    
-            </Typography>
-          </Toolbar>
-        </AppBar>
+          <AppBar
+            position="static"
+            style={{ marginBottom: "30px", background: "#e9edf2" }}
+          >
+            <Toolbar>
+              <img src={projectImage} height="50" />
+              <Typography variant="h5" className="page-title">
+                Project Detail
+              </Typography>
+            </Toolbar>
+          </AppBar>
           <ProjectNodeCard
             className="deviceNode"
             createDevice={this.props.createDevice}
-            deleteProject={(sid) => {
-              this.props.deleteProject(sid); browserHistory.push('/projects');
+            deleteProject={sid => {
+              this.props.deleteProject(sid);
+              browserHistory.push("/projects");
             }}
             permission={this.props.permission}
             project={project}
@@ -88,56 +112,33 @@ class ProjectDetail extends Component {
           />
           <Card className="deviceMap">
             <Typography>
-              <span className="Typography">
-                {' '}
-                Location
-                {' '}
-              </span>
-              {/* <LocationForm
-                handleClose={() => this.setState({ modalLocation: false })}
-                initialLocation={device.location}
-                modalOpen={this.state.modalLocation}
-                permission={this.props.permission}
-              /> */}
+              <span className="Typography">Device Locations </span>
             </Typography>
             <CardMedia>
-              <Map
+              <LeafletMap
                 ref="map"
-                center={position}
+                center={
+                  this.state.locations.length
+                    ? this.state.locations[0]
+                    : this.state.position
+                }
                 zoom={5}
               >
-                <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-                <Marker position={position}>
-                  <Popup>
-                    <span>
-                      Project Position
-                      <br />
-                      {' '}
-                      Latitude:
-                      {position[0]}
-                      {' '}
-                      <br />
-                      {' '}
-                      Longitude:
-                      {' '}
-                      {position[1]}
-                    </span>
-                  </Popup>
-                </Marker>
-              </Map>
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                />
+                {this.state.markers}
+              </LeafletMap>
             </CardMedia>
           </Card>
         </Container>
       );
     } else {
-      browserHistory.push('/projects');
+      browserHistory.push("/projects");
     }
 
-    return (
-      <div className="device">
-        {renderElement}
-      </div>
-    );
+    return <div className="device">{renderElement}</div>;
   }
 }
 
@@ -145,7 +146,9 @@ function mapStateToProps(state, ownProps) {
   return {
     project: state.project.project,
     projects: state.projects.projects,
-    permission: state.permissions.project.find(p => p.resource == ownProps.params.projectId),
+    permission: state.permissions.project.find(
+      p => p.resource == ownProps.params.projectId
+    ),
     user: state.current_user,
     devices: state.devices.devices,
     gateways: state.gateways.gateways
@@ -154,19 +157,22 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    getProject: (id) => {
+    getProject: id => {
       dispatch(getProject(id));
     },
-    getProjects: (params) => {dispatch(getProjects(params)) },
-    createDevice: (device) => {
+    getProjects: params => {
+      dispatch(getProjects(params));
+    },
+    createDevice: device => {
       dispatch(createDevice(device));
     },
-    deleteSensor: (sid, mid) => {
-      dispatch(deleteSensor(sid, mid));
+    getDevicePermissions: () => {
+      dispatch(getDevicePermissions());
     },
-    getDevicePermissions: () => {dispatch(getDevicePermissions()) }, 
-    getProjectPermissions: () => {dispatch(getProjectPermissions()) }, 
-    deleteProject: (id) => {
+    getProjectPermissions: () => {
+      dispatch(getProjectPermissions());
+    },
+    deleteProject: id => {
       dispatch(deleteProject(id));
     },
     updateProjectName: (id, n) => {
@@ -178,16 +184,16 @@ function mapDispatchToProps(dispatch) {
     updateProjectGateways: (id, n) => {
       dispatch(updateProjectGateways(id, n));
     },
-    updateSensorName: (deviceId, sensId, n) => {
-      dispatch(updateSensorName(deviceId, sensId, n));
-    },
-    getDevices: (params) => {
+    getDevices: params => {
       dispatch(getDevices(params));
     },
-    getGateways: (params) => {
+    getGateways: params => {
       dispatch(getGateways(params));
     }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectDetail);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProjectDetail);
