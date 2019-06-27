5d016a7a228a6f7d05214a902bd61235be75066b
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2019-06-27 11:29:06 +0300

    add actuator widget

--- src/components/devices/device/actuator/ActuatorCard.js
+++ src/components/devices/device/actuator/ActuatorCard.js
@@ -1,55 +1,282 @@
-import React, { Component } from 'react';
-import actuatorImage from '../../../../images/actuator.png';
-import PropTypes from 'prop-types';
-import Card from '@material-ui/core/Card';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import ActuatorForm from './ActuatorForm';
-import { Link } from 'react-router';
+import React, { Component } from "react";
+import actuatorImage from "../../../../images/actuator.png";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ActuatorForm from "./ActuatorForm";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import locale from "react-json-editor-ajrm/locale/en";
+import JSONInput from "react-json-editor-ajrm";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router";
+
+const styles = {
+  container: {
+    width: "100%"
+  },
+  body: {
+    width: undefined,
+    display: "flex"
+  },
+  outerBox: {
+    width: "100%",
+    minWidth: "250px"
+  },
+  contentBox: {
+    width: undefined,
+    flex: 1
+  },
+  warningBox: {
+    width: "100%"
+  },
+};
+
+const colors = {
+  default: "black",
+  background: "white",
+  background_warning: "black",
+  number: "black",
+  string: "red",
+  keys_whiteSpace: "black",
+  primitive: "black"
+}
 
 export default class ActuatorCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalEdit: false,
+      modalActuate: false,
+      actu: this.props.actuator ? this.props.actuator : defaultActu,
+      valid: true
     };
   }
 
+  componentWillMount() {
+    var actu = this.state.actu;
+    if(this.state.actu.actuator_value_type == 'bool'){
+      actu["value"] = Boolean(this.state.actu.value);
+    }
+    else if(this.state.actu.actuator_value_type == 'number'){
+      actu["value"] = Number(this.state.actu.value) || 0;
+    }
+    else if(this.state.actu.actuator_value_type == 'string'){
+      actu["value"] = String(this.state.actu.value) || '';
+    }
+    this.setState({ actu: actu });
+  }
+
+  handleChange = formData => {  
+    var actu = this.state.actu;
+    if(formData.target.name == 'numberValue'){
+      actu["value"] = Number(formData.target.value) || 0;
+    }
+    else
+    actu["value"] = formData.target.value;
+    this.setState({ actu: actu });
+  };
+
+  handleSwitchChange = event => {
+    var actu = this.state.actu;
+    actu.value = Boolean(event.target.checked);
+    this.setState({ actu: actu });
+  };
+
+
+  handleJSONInputChange = data => {
+    var actu = this.state.actu;
+    actu['value'] = data.jsObject;
+    this.setState({ actu: actu });
+    this.setState({ valid: !data.error });
+  };
+
   render() {
-    let actu = this.props.actuator
+    const { classes } = this.props;
+    let actu = this.props.actuator;
+    const sampleObject = {"key":"value"}
+    const sampleArray = ["one","two"]
 
     return (
       <Card className={"card "}>
-        <ActuatorForm modalOpen={this.state.modalEdit}
-                         handleClose={()=>{this.setState({modalEdit: false})}}
-                         onSubmit={(m) => {this.props.updateActuator(this.props.deviceId, m); this.setState({modalEdit: false});}}
-                         isEdit={true}
-                         actuator={actu}/>
+        <ActuatorForm
+          modalOpen={this.state.modalEdit}
+          handleClose={() => {
+            this.setState({ modalEdit: false });
+          }}
+          onSubmit={m => {
+            this.props.updateActuator(this.props.deviceId, m);
+            this.setState({ modalEdit: false });
+          }}
+          isEdit={true}
+          actuator={actu}
+        />
         <div className="TypographyDiv">
-          <pre className="Typography"> {actu.name? actu.name : "(" + actu.id + ")"} </pre>
-          <div className="cardTitleIcons"> 
-            {this.props.permission.scopes.includes("devices:update")? <EditIcon onClick={() => this.setState({modalEdit: true})}/>: null}
-            {this.props.permission.scopes.includes("devices:update")? <DeleteIcon onClick={() => {if(window.confirm('Delete actuator?')) this.props.deleteActuator(this.props.deviceId, actu.id)}}/>: null}
+          <pre className="Typography">
+            {" "}
+            {actu.name ? actu.name : "(" + actu.id + ")"}{" "}
+          </pre>
+          <div className="cardTitleIcons">
+            {this.props.permission.scopes.includes("devices:update") ? (
+              <EditIcon onClick={() => this.setState({ modalEdit: true })} />
+            ) : null}
+            {this.props.permission.scopes.includes("devices:update") ? (
+              <DeleteIcon
+                onClick={() => {
+                  if (window.confirm("Delete actuator?"))
+                    this.props.deleteActuator(this.props.deviceId, actu.id);
+                }}
+              />
+            ) : null}
           </div>
         </div>
         <div className="cardContent">
-          {this.props.isDetails? 
-          <div className="sensIcon">
-            <img src={actuatorImage} height="75"/>
-          </div> : null
-          }
-          {!this.props.isDetails? 
-            <Link to={"/devices/" + this.props.deviceId + "/actuators/" + actu.id} > 
-              <div className="actuIcon">
-                <img src={actuatorImage} height="75" title={"Go to actuator details"}/>
+          {this.props.isDetails ? (
+            <div>
+              <div className="sensIcon">
+                <img src={actuatorImage} height="75" />
               </div>
-            </Link> : null
-          }
+              {this.props.permission.scopes.includes("devices:update") ? (
+                <CardActions>
+                  <Grid
+                    container
+                    spacing={24}
+                    justify="flex-end"
+                    direction="row"
+                  >
+                    <Grid item xs={12}>
+                      {this.state.actu.actuator_value_type == "string" ? (
+                        <TextField
+                          id="stringValue"
+                          name="stringValue"
+                          label="Value"
+                          fullWidth
+                          value={this.state.actu.value}
+                          onChange={this.handleChange}
+                          helperText="Provide a valid string"
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {this.state.actu.actuator_value_type == "number" ? (
+                        <TextField
+                          id="numberValue"
+                          name="numberValue"
+                          label="Value"
+                          fullWidth
+                          value={this.state.actu.value}
+                          onChange={this.handleChange}
+                          helperText="Provide a valid number"
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {this.state.actu.actuator_value_type == "bool" ? (
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              id="boolValue"
+                              name="boolValue"
+                              checked={this.state.actu.value}
+                              onChange={this.handleSwitchChange}
+                              value={this.state.actu.value}
+                              color="primary"
+                            />
+                          }
+                          label="Value"
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {this.state.actu.actuator_value_type == "null" ? (
+                        <Button variant="contained">Click</Button>
+                      ) : (
+                        ""
+                      )}
+                      {this.state.actu.actuator_value_type == "object" ? (
+                        <Paper>
+                          <div style={{ maxWidth: "100%", maxHeight: "100%" }}>
+                          <JSONInput
+                            id="objectValue"
+                            name="objectValue"
+                            placeholder={sampleObject}
+                            onChange={this.handleJSONInputChange}
+                            theme="light_mitsuketa_tribute"
+                            locale={locale}
+                            colors={colors}
+                            height="150px"
+                            style={styles}
+                          />
+                          </div>
+                        </Paper>
+                      ) : (
+                        ""
+                      )}
+                      {this.state.actu.actuator_value_type == "array" ? (
+                        <Paper>
+                          <div style={{ maxWidth: "100%", maxHeight: "100%" }}>
+                          <JSONInput
+                            id="arrayValue"
+                            name="arrayValue"
+                            placeholder={sampleArray}
+                            onChange={this.handleJSONInputChange}
+                            theme="light_mitsuketa_tribute"
+                            locale={locale}
+                            colors={colors}
+                            height="150px"
+                            style={styles}
+                          />
+                          </div>
+                        </Paper>
+                      ) : (
+                        ""
+                      )}
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      container
+                      justify="flex-end"
+                      direction="row"
+                    >
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="primary"
+                        disabled={!this.state.valid}
+                        onTouchTap={() => {
+                          this.props.updateActuator(this.props.deviceId, this.state.actu);
+                        }}
+                      >
+                        Submit
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </CardActions>
+              ) : null}
+            </div>
+          ) : null}
+          {!this.props.isDetails ? (
+            <Link
+              to={"/devices/" + this.props.deviceId + "/actuators/" + actu.id}
+            >
+              <div className="actuIcon">
+                <img
+                  src={actuatorImage}
+                  height="75"
+                  title={"Go to actuator details"}
+                />
+              </div>
+            </Link>
+          ) : null}
         </div>
       </Card>
     );
   }
-  
+
   static propTypes = {
     actu: PropTypes.object, //Should be a Waziup.Actuator
     isEditable: PropTypes.bool,
@@ -58,6 +285,5 @@ export default class ActuatorCard extends Component {
     deleteActuator: PropTypes.func,
     deviceId: PropTypes.string.isRequired,
     permission: PropTypes.object.isRequired
-  }
+  };
 }
-
--- src/components/devices/device/actuator/ActuatorForm.js
+++ src/components/devices/device/actuator/ActuatorForm.js
@@ -1,95 +1,150 @@
-import React, {Component} from 'react';
-import { reduxForm, Field } from 'redux-form'
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField'
-import PropTypes from 'prop-types';
-import * as Waziup from 'waziup-js'
-import deviceImage from '../../../../images/actuator.png';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import TextField from "@material-ui/core/TextField";
+import PropTypes from "prop-types";
+import * as Waziup from "waziup-js";
+import deviceImage from "../../../../images/actuator.png";
 
 class ActuatorForm extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    var defaultActu = new Waziup.Actuator("MOT")
-    defaultActu.name = "DC Motor"
-    defaultActu.actuator_kind = "Motor"
-    defaultActu.actuator_value_type = "bool"
-    defaultActu.value = true
+    var defaultActu = new Waziup.Actuator("MOT");
+    defaultActu.name = "DC Motor";
+    defaultActu.actuator_kind = "Motor";
+    defaultActu.actuator_value_type = "bool";
+    defaultActu.value = true;
     this.state = {
-      actu: (this.props.actuator? this.props.actuator: defaultActu)
+      actu: this.props.actuator ? this.props.actuator : defaultActu
     };
   }
-  
-  handleChange = (formData) => {
-    var actu = this.state.actu
-    actu[formData.target.name] = formData.target.value;
-    this.setState({actu: actu})
-  }
 
-  handleSelect = (field) => {
+  handleChange = formData => {
+    var actu = this.state.actu;
+    actu[formData.target.name] = formData.target.value;
+    this.setState({ actu: actu });
+  };
+
+  handleSelect = field => {
     return event => {
-      var actu = this.state.actu
+      var actu = this.state.actu;
       actu[field] = event.target.value;
-      this.setState({actu: actu});
-    }
-  }
+      this.setState({ actu: actu });
+    };
+  };
 
   render() {
     const actions = [
-      <Button color="primary" key='cancel' onTouchTap={()=>{this.props.handleClose();}}>Cancel</Button>,
-      <Button color="primary" key='submit' onTouchTap={()=>{this.props.onSubmit(this.state.actu); this.props.handleClose();}}>Submit</Button>,
+      <Button
+        color="primary"
+        key="cancel"
+        onTouchTap={() => {
+          this.props.handleClose();
+        }}
+      >
+        Cancel
+      </Button>,
+      <Button
+        color="primary"
+        key="submit"
+        onTouchTap={() => {
+          this.props.onSubmit(this.state.actu);
+          this.props.handleClose();
+        }}
+      >
+        Submit
+      </Button>
     ];
-    const valueTypes = ["string", "number", "bool", "null", "object","array"]
-    const ActuatorKinds = ["Motor","Lamp"]
+    const valueTypes = ["string", "number", "bool", "null", "object", "array"];
+    const ActuatorKinds = ["Motor", "Lamp"];
     return (
-      <Dialog actions={actions} modal={true} open={this.props.modalOpen} autoScrollBodyContent={true}>
-         <DialogTitle>{this.props.isEdit? "Edit Actuator": "Add actuator"}</DialogTitle>
-         <DialogContent>
-         <img src={deviceImage} height="100"/>
-         <div className="locationCoords">
-         <Grid container spacing={24}>
-        <Grid item xs={6}>
-           <TextField name="id" disabled={this.props.isEdit} label="ID" value={this.state.actu.id} onChange={this.handleChange} title="Short ID used by the Gateway to send the actuure"/>
-           </Grid>
-        <Grid item xs={6}>
-           <TextField name="name" label="Name" value={this.state.actu.name} onChange={this.handleChange} title="A name for this device actuator"/>
-           </Grid>
-        <Grid item xs={6}>
-          <FormControl>
-            <InputLabel htmlFor="actuator_kind">Actuator kind</InputLabel>
-            <Select style={{ minWidth: 150 }}
-            input={<Input name="actuator_kind" id="actuator_kind" />}
-            value={this.state.actu.actuator_kind} onChange={this.handleSelect("actuator_kind")} >
-              {ActuatorKinds.map(ak => <MenuItem key={ak} value={ak}>{ak}</MenuItem>)}
-           </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-        <FormControl>
-            <InputLabel htmlFor="actuator_value_type">Value Type</InputLabel>
-            <Select style={{ minWidth: 150 }}
-            input={<Input name="actuator_value_type" id="actuator_value_type" />}
-            value={this.state.actu.actuator_value_type} onChange={this.handleSelect("actuator_value_type")}>
-              {valueTypes.map(u => <MenuItem key={u} value={u}>{u}</MenuItem>)}
-           </Select>
-          </FormControl>        
-           </Grid>
-           </Grid>
-        </div>
+      <Dialog
+        actions={actions}
+        modal={true}
+        open={this.props.modalOpen}
+        autoScrollBodyContent={true}
+      >
+        <DialogTitle>
+          {this.props.isEdit ? "Edit Actuator" : "Add actuator"}
+        </DialogTitle>
+        <DialogContent>
+          <img src={deviceImage} height="100" />
+          <div className="locationCoords">
+            <Grid container spacing={24}>
+              <Grid item xs={6}>
+                <TextField
+                  name="id"
+                  disabled={this.props.isEdit}
+                  label="ID"
+                  value={this.state.actu.id}
+                  onChange={this.handleChange}
+                  title="Short ID used by the Gateway to send the actuure"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  name="name"
+                  label="Name"
+                  value={this.state.actu.name}
+                  onChange={this.handleChange}
+                  title="A name for this device actuator"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl>
+                  <InputLabel htmlFor="actuator_kind">Actuator kind</InputLabel>
+                  <Select
+                    style={{ minWidth: 150 }}
+                    input={<Input name="actuator_kind" id="actuator_kind" />}
+                    value={this.state.actu.actuator_kind}
+                    onChange={this.handleSelect("actuator_kind")}
+                  >
+                    {ActuatorKinds.map(ak => (
+                      <MenuItem key={ak} value={ak}>
+                        {ak}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl>
+                  <InputLabel htmlFor="actuator_value_type">
+                    Value Type
+                  </InputLabel>
+                  <Select
+                    style={{ minWidth: 150 }}
+                    input={
+                      <Input
+                        name="actuator_value_type"
+                        id="actuator_value_type"
+                      />
+                    }
+                    value={this.state.actu.actuator_value_type}
+                    onChange={this.handleSelect("actuator_value_type")}
+                  >
+                    {valueTypes.map(u => (
+                      <MenuItem key={u} value={u}>
+                        {u}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </div>
         </DialogContent>
-        <DialogActions>
-            {actions}
-        </DialogActions>
+        <DialogActions>{actions}</DialogActions>
       </Dialog>
     );
   }
@@ -100,10 +155,9 @@ class ActuatorForm extends Component {
     isEdit: PropTypes.bool.isRequired,
     handleClose: PropTypes.func.isRequired,
     onSubmit: PropTypes.func.isRequired
-  }
+  };
 }
 
-
 export default reduxForm({
-    form: 'simple'
-})(ActuatorForm)
+  form: "simple"
+})(ActuatorForm);
