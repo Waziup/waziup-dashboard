42bca20f8790bb0babf780a8f4d3b3550a0f8290
Corentin Dupont <corentin.dupont@gmail.com>
2021-09-29 18:00:09 +0200

    project bug

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { Container } from "react-grid-system";
