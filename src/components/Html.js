c329334cf409a97dbc13292571ea14e48c66a7dd
Corentin Dupont <corentin.dupont@gmail.com>
2022-01-28 12:37:53 +0100

    relative assets

--- src/components/Html.js
+++ src/components/Html.js
@@ -17,14 +17,14 @@ class Html extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link key={vendor} rel="preload" href={vendor} as="script" />
           <link key={client} rel="preload" href={client} as="script" />
-          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-          <link rel="stylesheet" href="/index.css" />
+          <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
+          <link rel="stylesheet" href="./index.css" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.css"/>
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.css"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.3/toastr.min.css"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:500"></link>
-          <link rel="stylesheet" href="/style.css"/>
+          <link rel="stylesheet" href="./style.css"/>
         </head>
         <body>
           <div id="app" className="content-wrap" dangerouslySetInnerHTML={{ __html: children }} />
